ebcc7d4aa471ffbf9a7db3763ab68e172e328d9c
Karel Zimmer <karel.zimmer@gmail.com>
2022-09-26 09:16:25 +0200

    Moved instrcutions to README

--- README.md
+++ README.md
@@ -2,3 +2,36 @@
 
 Mijn Rust-programma's.
 Voor (zelf)studie. Werk in uitvoering.
+
+## Werkwijze
+
+```console
+cd ~/rust
+cd hello-world
+```
+
+Edit main.rs:
+
+```rust
+fn main() {
+    println!("Hello, world!");
+}
+```
+
+Compile:
+
+```console
+rustc main.rs
+```
+
+Run:
+
+```console
+./main
+```
+
+Output:
+
+```console
+Hello, world!
+```
--- hello_world/main.rs
+++ hello_world/main.rs
@@ -1,6 +1,7 @@
-// Hello, World!
-// Compile: rustc main.rs
-// Run:     ./main
+//#############################################################################
+// https://doc.rust-lang.org/stable/book/ch01-02-hello-world.html
+// Your first Rust program
+//#############################################################################
 fn main() {
     println!("Hello, world!");
 }
